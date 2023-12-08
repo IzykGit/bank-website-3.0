c32568d93146e6a32366e5b739dd4a052f724658
Izyk <lance_hemphill1234@yahoo.com>
2023-12-08 11:57:46 -0500

    added info cards under fold, starting popular cards section, added images, added text, completed info card section

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -2,6 +2,10 @@ import styles from '../styles/landingPage.module.css'
 import Navbar from '../components/navbar'
 import Fold from '../components/fold'
 import TopBar from '../components/topBar'
+import laptopGraph from 'C:/Users/lance/Desktop/Developement/Coding/bank-app-3/src/assets/laptopGraph.jpg'
+import bankFront from 'C:/Users/lance/Desktop/Developement/Coding/bank-app-3/src/assets/bankFront.jpg'
+import phoneMoney from 'C:/Users/lance/Desktop/Developement/Coding/bank-app-3/src/assets/phoneMoney.jpg'
+import cardOnLaptop from 'C:/Users/lance/Desktop/Developement/Coding/bank-app-3/src/assets/cardOnLaptop.jpg'
 
 export default function LandingPage() {
 
@@ -18,14 +22,56 @@ export default function LandingPage() {
             </div>
             <div className={styles.infoCardsContainer}>
                 <div className={styles.infoCard}>
-
+                        <div className={styles.cardText}>
+                            <p>Seamless Online Banking Experience:</p>
+                            <p>Experience the convenience of banking at your fingertips with our secure and user-friendly online banking platform.
+                            Manage your accounts, track transactions, and stay in control of your finances 24/7 from the comfort of your home or on the go.</p>
+                            <button>View Account</button>
+                        </div>
+                    <img alt="graph on laptop" src={laptopGraph}/>
                 </div>
                 <div className={styles.infoCard}>
-                    
+                        <div className={styles.cardText}>
+                            <p>Knowledge is Power</p>
+                            <p>Knowledge empowers. Explore our collection of educational resources and articles to enhance your financial literacy.
+                            Best Bank is dedicated to ensuring that you have the tools and information you need to make sound financial decisions.</p>
+                            <button>Explore Resources</button>
+                        </div>
+                    <img alt="front of bank" src={bankFront}/>
                 </div>
                 <div className={styles.infoCard}>
-                    
+                        <div className={styles.cardText}>
+                            <p>We Offer 5% APY</p>
+                            <p>Introducing Best Bank's High-Yield Savings Account, offering an impressive 5% Annual Percentage Yield (APY).
+                            At Best Bank, we believe in helping you maximize the growth of your hard-earned money while providing a secure and reliable banking experience.</p>
+                            <button>Get Quote</button>
+                        </div>
+                    <img alt="phone with money symbol" src={phoneMoney} />
                 </div>
+            </div>
+            <div className={styles.mainCardOptionContainer}>
+                <div className={styles.secondaryCardOptionContainer}>
+                    <p><span style={{color: "#47769E", backgroundColor: "white"}}>Explore</span> our Most Popular card options!</p>
+                        <div className={styles.cardOption}>
+                            <p>Cashback Credit Card:</p>
+                            <p>Earn while you spend! Introducing our Cashback Credit Card – get a percentage of your purchases back in cash rewards.
+                            Enjoy the simplicity of straightforward benefits with every swipe.</p>
+                            <button>More Info</button>
+                        </div>
+                        <div className={styles.cardOption}>
+                            <p>Travel Rewards Credit Card:</p>
+                            <p>Explore the world with our Travel Rewards Credit Card! Earn points on every purchase,
+                            redeemable for exciting travel perks such as airline miles, hotel stays, and more. Your passport to a world of possibilities awaits.</p>
+                            <button>More Info</button>
+                        </div>
+                        <div className={styles.cardOption}>
+                            <p>Credit Builder Credit Card:</p>
+                            <p>"Build a brighter financial future with our Credit Builder Credit Card.
+                            Designed to help you establish or improve your credit history, this card is your key to unlocking better financial opportunities."</p>
+                            <button>More Info</button>
+                        </div>
+                        <img alt="visa card on laptop" src={cardOnLaptop} className={styles.cardOptions}/>                  
+                    </div>
             </div>
         </div>
     )
